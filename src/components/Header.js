6f91d13348bc52fc3528ec1a644bbc5d01794d99
Jack McConnell <jackmc1122@gmail.com>
2019-08-18 15:50:38 -0700

    Add drawer to hold the channels

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,29 @@ import React from 'react';
 
 import Navigation from './Navigation';
 
-import styles from './Header.css';
-
 import { Toolbar, AppBar, IconButton, Typography } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
 const Header = () => {
+  const classes = useStyles();
   return (
-    <AppBar className={styles.Header} position='sticky'>
+    <AppBar className={classes.appBar} position='sticky'>
       <Toolbar>
-        <IconButton edge="start" color="inherit" aria-lable="menu">
+        <IconButton edge="start" color="inherit">
           <MenuIcon></MenuIcon>
         </IconButton>
-        <Typography className={styles.Title} variant="h6">
+        <Typography className={classes.title} variant="h6">
           Jabberish
         </Typography>
         <Navigation/>
--- src/components/Workspace/ChannelList.js
+++ src/components/Workspace/ChannelList.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ChannelItem from './ChannelItem';
 
 import styles from './ChannelList.css';
+import { Drawer, List, ListItem, ListItemText } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  toolbar: theme.mixins.toolbar,
+}));
+
 
 const ChannelList = ({ channels, selectChannel }) => {
-
-  const channelItems = channels.map(channel => {
-    return (
-      <ChannelItem 
-        key={`${channel._id}-${channel.name}`} 
-        channel={channel} 
-        selectChannel={selectChannel}
-      />
-    );
-  });
+  
+  const classes = useStyles();
   
   return (
-    <ul className={styles.ChannelList}>
-      Channel List
-      {channelItems}
-    </ul>
+    <Drawer variant="permanent" className={styles.ChannelList}>
+      <div className={classes.toolbar} />
+
+      Channels
+      <List>
+        {channels.map(channel => (
+          <ListItem 
+            button 
+            key={channel._id}
+            onClick={() => selectChannel(channel)}
+          >
+            <ListItemText primary={channel.name} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
   );
 };
 
