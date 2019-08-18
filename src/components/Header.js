eab89879d112ba3cf78616f31e788068f9331378
Jack McConnell <jackmc1122@gmail.com>
2019-08-18 15:04:44 -0700

    Make the header sticky

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ import MenuIcon from '@material-ui/icons/Menu';
 
 const Header = () => {
   return (
-    <AppBar className={styles.Header}>
+    <AppBar className={styles.Header} position='sticky'>
       <Toolbar>
         <IconButton edge="start" color="inherit" aria-lable="menu">
           <MenuIcon></MenuIcon>
