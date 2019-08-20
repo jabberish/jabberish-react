66258db7814d352c3da20fe89accf91707fa144c
Jackmc <jackmc1122@gmail.com>
2019-08-20 10:37:18 -0700

    Update LoginForm to make it a functional component.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,49 @@
 import React from 'react';
+
 import LoginForm from '../containers/LoginForm';
 
-const Login = () => {
-  return (
-    <>
-      <h1>Login</h1>
-      <LoginForm />
-    </>
-  );
-};
+import { fetchLogin } from '../services/auth-api';
+
+class Login extends React.Component {
+  state = {
+    username: '',
+    password: '',
+    loginSuccess: true
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const user = {
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    fetchLogin(user)
+      .then(res => {
+        if(res._id) {
+          window.location = '/';
+        } else {
+          this.setState({ loginSuccess: false });
+        }
+      });
+  };
+
+  handleUpdate = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  
+  render() {
+    return (
+      <>
+        <h1>Login</h1>
+        <LoginForm 
+          handleSubmit={this.handleSubmit}
+          handleUpdate={this.handleUpdate}
+          loginSuccess={this.state.loginSuccess}
+        />
+      </>
+    );
+  }
+}
 
 export default Login;
--- src/containers/LoginForm.js
+++ src/containers/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fetchLogin } from '../services/auth-api';
+import PropTypes from 'prop-types';
 
 const formStyle = {
   display: 'flex',
@@ -11,65 +11,38 @@ const labelStyle = {
   marginBottom: '10px'
 };
 
-class LoginForm extends React.Component {
-  state = {
-    username: '',
-    password: '',
-    loginSuccess: true
-  }
+const LoginForm = ({ handleSubmit, handleUpdate, loginSuccess }) => {
+  return (
+    <form id="login-form" style={formStyle}>
+      <label style={labelStyle}>username:
+        <input 
+          id="username" 
+          name="username" 
+          onChange={handleUpdate}
+          required 
+        />
+      </label>
+      <label style={labelStyle}>Password:
+        <input 
+          id="password" 
+          name="password" 
+          type="password" 
+          onChange={handleUpdate}
+          required 
+        />
+      </label>
+      {!loginSuccess && (
+        <span>Incorrect username or password!</span>
+      )}
+      <button onClick={handleSubmit} style={{ width: '5rem' }}>Submit</button>
+    </form>
+  );
+};
 
-  onSubmit = e => {
-    e.preventDefault();
-    const user = {
-      username: this.state.username,
-      password: this.state.password
-    };
-
-    fetchLogin(user)
-      .then(res => {
-        if(res._id) {
-          window.location = '/';
-        } else {
-          this.setState({ loginSuccess: false });
-        }
-      });
-  };
-
-  onUsernameChange = e => {
-    this.setState({ username: e.target.value });
-  }
-
-  onPasswordChange = e => {
-    this.setState({ password: e.target.value });
-  }
-
-  render() {
-    return (
-      <form id="login-form" style={formStyle}>
-        <label style={labelStyle}>username:
-          <input 
-            id="username" 
-            name="username" 
-            onChange={this.onUsernameChange}
-            required 
-          />
-        </label>
-        <label style={labelStyle}>Password:
-          <input 
-            id="password" 
-            name="password" 
-            type="password" 
-            onChange={this.onPasswordChange}
-            required 
-          />
-        </label>
-        {!this.state.loginSuccess && (
-          <span>Incorrect username or password!</span>
-        )}
-        <button onClick={this.onSubmit} style={{ width: '5rem' }}>Submit</button>
-      </form>
-    );
-  }
-}
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleUpdate: PropTypes.func.isRequired,
+  loginSuccess: PropTypes.bool.isRequired
+};
 
 export default LoginForm;
