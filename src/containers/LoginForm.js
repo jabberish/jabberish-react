87c00eff78ac41e33b3bffcbe64af63133674d37
Jackmc <jackmc1122@gmail.com>
2019-08-20 10:52:43 -0700

    Update Login page to be more material ui like.

--- src/containers/LoginForm.js
+++ src/containers/LoginForm.js
@@ -1,41 +1,92 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const formStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center'
-};
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-const labelStyle = {
-  marginBottom: '10px'
-};
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const LoginForm = ({ handleSubmit, handleUpdate, loginSuccess }) => {
+  const classes = useStyles();
   return (
-    <form id="login-form" style={formStyle}>
-      <label style={labelStyle}>username:
-        <input 
-          id="username" 
-          name="username" 
-          onChange={handleUpdate}
-          required 
-        />
-      </label>
-      <label style={labelStyle}>Password:
-        <input 
-          id="password" 
-          name="password" 
-          type="password" 
-          onChange={handleUpdate}
-          required 
-        />
-      </label>
-      {!loginSuccess && (
-        <span>Incorrect username or password!</span>
-      )}
-      <button onClick={handleSubmit} style={{ width: '5rem' }}>Submit</button>
-    </form>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            onChange={handleUpdate}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={handleUpdate}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleSubmit}
+          >
+            Sign In
+          </Button>
+          {!loginSuccess && (
+            <span>Incorrect username or password!</span>
+          )}
+        </form>
+      </div>
+    </Container>
   );
 };
 
